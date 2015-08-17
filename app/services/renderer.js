96ac585505213b0b799c178ce203e3a0ecbf997f
Tommy Conway <tmycnw@gmail.com>
2015-08-17 19:09:55 +0530

    adding some scaffolding code in the renderer service

--- app/services/renderer.js
+++ app/services/renderer.js
@@ -5,8 +5,40 @@
 
     function () {
 
-      return {
+      var canvas, renderPending = false;
 
+      function init(c) {
+
+        canvas = c;
+
+        renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+        renderer.setClearColor(0xffffff);
+      }
+
+      function render() {
+
+        //renderer.render(scene, camera);
+        renderPending = false;
+      }
+
+      function requestRender() {
+        if (renderPending) {
+          return;
+        }
+
+        window.requestAnimationFrame(render);
+        renderPending = true;
+      }
+
+      function resize() {
+
+        requestRender();
+      }
+
+      return {
+        init: init,
+        resize: resize,
+        requestRender: requestRender
       };
     }
   ]);
