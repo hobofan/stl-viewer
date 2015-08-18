dd0c7a529afc03a479ddfee293890f5e55474a50
Tommy Conway <tmycnw@gmail.com>
2015-08-18 19:00:33 +0530

    adding a dummy canvas controller test

---
+++ tests/controllers/canvasCtrl.js
@@ -0,0 +1,12 @@
+
+describe('CanvasCtrl', function () {
+
+  beforeEach(module('stlApp'));
+
+  it('should do something', inject(function ($controller) {
+
+    var scope = {}, ctrl = $controller('CanvasCtrl', {$scope:scope});
+
+    //expect(scope.str).toBeDefined();
+  }));
+});
