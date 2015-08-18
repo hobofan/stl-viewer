3e10c3bf83a2bfdb6f8f6fde862cb66fc24dadd7
Tommy Conway <tmycnw@gmail.com>
2015-08-18 19:05:27 +0530

    replacing the menu function in the main controller with the menu service

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -2,40 +2,6 @@
 (function () {
   "use strict";
 
-  function createMenu() {
-
-    if (typeof require === 'undefined') {
-      return;
-    }
-
-    var remote = require('remote');
-    var Menu = remote.require('menu');
-    var Dialog = remote.require('dialog');
-
-    var template = [
-     {
-        label: 'File',
-        submenu: [
-          {
-            label: 'Open',
-            click: function() {
-              Dialog.showOpenDialog(remote.getCurrentWindow(), {
-                properties: [ 'openFile'],
-                filters: [{ name: 'STL', extensions: ['stl'] }]},
-                function (fileName) {
-                  console.log(fileName);
-                }
-              );
-            }
-          }
-       ]
-     }
-   ];
-
-    var menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-  }
-
   function setupWindowEventHandlers($window, renderer) {
 
     angular.element($window).bind('resize', function () {
@@ -44,11 +10,11 @@
     });    
   }
 
-  app.controller('MainCtrl', ['$scope', '$window', 'stlRenderer',
+  app.controller('MainCtrl', ['$scope', '$window', 'stlMenu', 'stlRenderer',
 
-    function($scope, $window, stlRenderer) {
+    function($scope, $window, stlMenu, stlRenderer) {
       
-      createMenu();
+      stlMenu.create();
       setupWindowEventHandlers($window, stlRenderer);
     }
   ]);
