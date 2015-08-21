c86d300f9ca675f39cc0a89640dd9970c185ed53
Tommy Conway <tmycnw@gmail.com>
2015-08-21 17:52:04 +0530

    adding a mode property to the camera and functions to get and set the mode

--- app/services/camera.js
+++ app/services/camera.js
@@ -7,13 +7,31 @@
 
     	var camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
 
+			var modes = {
+        ROTATE: "rotate",
+        PAN: "pan",
+        ZOOM: "zoom"
+      },
+      mode = modes.ROTATE;
+
+      function setMode(m) {
+      	mode = m;
+      }
+
+      function getMode() {
+      	return mode;
+      }
+
       function update(aspect, bbox) {
 
       }
 
       return {
       	camera: camera,
-      	update: update
+      	update: update,
+      	modes: modes,
+      	setMode: setMode,
+      	getMode: getMode
       };
     }
   ]);
--- tests/services/camera.js
+++ tests/services/camera.js
@@ -9,4 +9,25 @@ describe('StlCamera', function () {
     expect(stlCamera.camera).toBeDefined();
     expect(angular.isFunction(stlCamera.update)).toBe(true);
   }));
+
+  it('should have a modes enum and functions to get and set the mode',
+  	inject(function (stlCamera) {
+
+		expect(stlCamera.modes).toBeDefined();
+    expect(angular.isFunction(stlCamera.setMode)).toBe(true);
+    expect(angular.isFunction(stlCamera.getMode)).toBe(true);
+  }));
+
+  it('should return the mode that was set earlier',
+  	inject(function (stlCamera) {
+
+		stlCamera.setMode(stlCamera.modes.PAN);
+    expect(stlCamera.getMode()).toEqual(stlCamera.modes.PAN);
+  }));
+
+  it('should set the mode to rotate by default',
+  	inject(function (stlCamera) {
+
+    expect(stlCamera.getMode()).toEqual(stlCamera.modes.ROTATE);
+  }));
 });
