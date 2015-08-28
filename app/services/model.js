3789a60fe08172ba1548885b12664da4b8ef73db
Tommy Conway <tmycnw@gmail.com>
2015-08-28 18:19:30 +0530

    using the material service in the stl model service to set the material for the mesh

--- app/services/model.js
+++ app/services/model.js
@@ -1,9 +1,9 @@
 (function () {
   "use strict";
 
-  app.service('stlModel', [
+  app.service('stlModel', ['stlMaterials',
 
-    function () {
+    function (stlMaterials) {
 
       function open(data, callback) {
 
@@ -11,19 +11,10 @@
 
           if (v && v.length > 3) {
 
-            var material = new THREE.MeshLambertMaterial({
-              color: 0xccc,
-              side: THREE.DoubleSide
-            });
-
-            material.polygonOffset = true;
-            material.polygonOffsetFactor = 1.0;
-            material.polygonOffsetUnits = 1.0;
-
             var geometry = new THREE.BufferGeometry();
             geometry.addAttribute('position', new THREE.BufferAttribute(v, 3));
             geometry.addAttribute('normal', new THREE.BufferAttribute(n, 3));
-            var mesh = new THREE.Mesh(geometry, material);
+            var mesh = new THREE.Mesh(geometry, stlMaterials.defaultMaterial);
 
             if (callback) {
               callback();
