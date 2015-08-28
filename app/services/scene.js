0f008c357c8c91ecb95d77fecdde9bed69de4fa5
Tommy Conway <tmycnw@gmail.com>
2015-08-28 18:42:47 +0530

    implementing the scene service

--- app/services/scene.js
+++ app/services/scene.js
@@ -5,8 +5,30 @@
 
     function () {
 
-      return {
+      var scene = new THREE.Scene();
+      var bbox = new THREE.Box3().setFromObject(scene);
 
+      function getBox() {
+        return bbox;
+      }
+
+      function add(obj) {
+
+        scene.add(obj);
+        bbox.setFromObject(scene);
+      }
+
+      function remove(obj) {
+
+        scene.remove(obj);
+        bbox.setFromObject(scene);
+      }
+
+      return {
+        scene: scene,
+        getBox: getBox,
+        add: add,
+        remove: remove
       };
     }
   ]);
--- tests/services/scene.js
+++ tests/services/scene.js
@@ -3,7 +3,17 @@ describe('StlScene', function () {
 
   beforeEach(module('stlApp'));
 
-  it('should do something', inject(function (stlScene) {
-    expect(true).toBe(true);
+  it('should have a scene property of the correct type', inject(function (stlScene) {
+    expect(stlScene.scene).toBeDefined();
+    expect(stlScene.scene instanceof THREE.Scene).toBeDefined();
+  }));
+
+  it('should have an add and a remove function', inject(function (stlScene) {
+    expect(angular.isFunction(stlScene.add)).toBe(true);
+    expect(angular.isFunction(stlScene.remove)).toBe(true);
+  }));
+
+  it('should have a function to get the bounding box', inject(function (stlScene) {
+    expect(angular.isFunction(stlScene.getBox)).toBe(true);
   }));
 });
