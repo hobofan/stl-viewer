24261359217466a877d03136136e1f79a3bbc938
Tommy Conway <tmycnw@gmail.com>
2015-08-29 15:44:09 +0530

    using the stlbuffer service in the file open service

--- app/services/ui/fileOpen.js
+++ app/services/ui/fileOpen.js
@@ -1,20 +1,11 @@
 (function () {
   "use strict";
 
-  app.service('stlFileOpen', ['stlModel',
+  app.service('stlFileOpen', ['stlModel', 'stlBuffer',
 
-    function (stlModel) {
+    function (stlModel, stlBuffer) {
 
       function open() {
-
-        function toArrayBuffer(buffer) {
-          var ab = new ArrayBuffer(buffer.length);
-          var view = new Uint8Array(ab);
-          for (var i = 0; i < buffer.length; ++i) {
-            view[i] = buffer[i];
-          }
-          return ab;
-        }
 
         var remote = require('remote');
         var menu = remote.require('menu');
@@ -31,7 +22,7 @@
                   return;
                 }
 
-                stlModel.open(toArrayBuffer(data));
+                stlModel.open(stlBuffer.toArrayBuffer(data));
               });
             }
           }
