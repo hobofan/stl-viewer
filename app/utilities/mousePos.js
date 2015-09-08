2439d4448ac4b61bd5d41c96e0d2e0b6cfc3c157
Tommy Conway <tmycnw@gmail.com>
2015-09-08 18:45:33 +0530

    implementing the pos function

--- app/utilities/mousePos.js
+++ app/utilities/mousePos.js
@@ -5,8 +5,14 @@
 
     function () {
 
+      var pixelRatio = window.devicePixelRatio || 1;
+
       function pos(ev) {
 
+        return {
+          X: ev.layerX * pixelRatio,
+          Y: ev.layerY * pixelRatio
+        };
       }
 
       return {
--- tests/utilities/mousePos.js
+++ tests/utilities/mousePos.js
@@ -9,4 +9,15 @@ describe('StlMousePos', function () {
       expect(angular.isFunction(stlMousePos.pos)).toBe(true);
     }
   ));
+
+  it('should return a valid position object from the pos function', inject(
+    function (stlMousePos) {
+
+      var pixelRatio = window.devicePixelRatio || 1;
+      var pos = stlMousePos.pos({ layerX: 1, layerY: 2});
+
+      expect(pos.X).toEqual(1* pixelRatio);
+      expect(pos.Y).toEqual(2* pixelRatio);
+    }
+  ));
 });
