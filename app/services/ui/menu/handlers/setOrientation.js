9625420bfb40bbb4d4bcd439062b6e25d9553df2
Tathagata Chakraborty <tatx@me.com>
2015-09-20 13:10:34 +0530

    implementing the set orientation service

--- app/services/ui/menu/handlers/setOrientation.js
+++ app/services/ui/menu/handlers/setOrientation.js
@@ -2,13 +2,15 @@
 (function () {
   "use strict";
 
-  app.service('stlSetOrientation', [
+  app.service('stlSetOrientation', ['stlCamera', 'stlOrient', 'stlRenderer',
 
-    function () {
+    function (stlCamera, stlOrient, stlRenderer) {
 
       function set(item) {
 
-        var orientation = item.label.toLowerCase();
+        var orientation = item.label.toUpperCase();
+        stlCamera.orient(stlOrient.orient(stlOrient.orientations[orientation]));
+        stlRenderer.render();
       }
 
       return {
