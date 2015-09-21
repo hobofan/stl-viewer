720de468cab661fea7030d64d40bd18121cf9995
Tathagata Chakraborty <tatx@me.com>
2015-09-21 17:54:58 +0530

    adding a status controller

---
+++ app/controllers/statusCtrl.js
@@ -0,0 +1,7 @@
+(function () {
+  "use strict";
+
+  app.controller('StatusCtrl', function($scope) {
+    $scope.str = "some test string";
+  });
+}());
---
+++ tests/controllers/statusCtrl.js
@@ -0,0 +1,12 @@
+
+describe('StatusCtrl', function () {
+
+  beforeEach(module('stlApp'));
+
+  it('should do something', inject(function ($controller) {
+
+    var scope = {}, ctrl = $controller('StatusCtrl', {$scope:scope});
+
+    //expect(scope.str).toBeDefined();
+  }));
+});
