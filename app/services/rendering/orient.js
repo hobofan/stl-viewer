fcb4112a92cb46d7abfa2856505df3af57e5b0e4
Tathagata Chakraborty <tatx@me.com>
2015-09-29 18:21:51 +0530

    now showing the proper orientation name and also updating the orientation value in the status

--- app/services/rendering/orient.js
+++ app/services/rendering/orient.js
@@ -6,20 +6,20 @@
     function () {
 
       var orientations = {
-        BACK: { yaw: Math.PI / 2, roll: Math.PI / 2 },
-        FRONT: { yaw: -Math.PI / 2, roll: Math.PI / 2 },
-        RIGHT: { yaw: Math.PI * 2, roll: Math.PI / 2 },
-        LEFT: { yaw: Math.PI, roll: Math.PI / 2 },
-        TOP: { yaw: 0, roll: 0 },
-        BOTTOM: { yaw: Math.PI, roll: Math.PI },
-        ISOMETRIC: { yaw: Math.PI / 4.0, roll: Math.PI / 3.0 }
+        BACK: { yaw: Math.PI / 2, roll: Math.PI / 2, name: "Back" },
+        FRONT: { yaw: -Math.PI / 2, roll: Math.PI / 2, name: "Front" },
+        RIGHT: { yaw: Math.PI * 2, roll: Math.PI / 2, name: "Right" },
+        LEFT: { yaw: Math.PI, roll: Math.PI / 2, name: "Left" },
+        TOP: { yaw: 0, roll: 0, name: "Top" },
+        BOTTOM: { yaw: Math.PI, roll: Math.PI, name: "Bottom" },
+        ISOMETRIC: { yaw: Math.PI / 4.0, roll: Math.PI / 3.0, name: "Isometric" }
       };
 
-      var lastOrient = orientations.ISOMETRIC;
+      var lastOrient = orientations.ISOMETRIC.name;
 
       function orient(o) {
 
-        lastOrient = o;
+        lastOrient = o.name;
 
         var rot = new THREE.Quaternion();
         rot.multiply(quatFromAxisAngle(vec3(0, 0, 1), o.yaw));
--- app/services/ui/menu/handlers/setOrientation.js
+++ app/services/ui/menu/handlers/setOrientation.js
@@ -3,14 +3,17 @@
   "use strict";
 
   app.service('stlSetOrientation', ['stlCamera', 'stlOrient', 'stlRenderer',
+    '$rootScope',
 
-    function (stlCamera, stlOrient, stlRenderer) {
+    function (stlCamera, stlOrient, stlRenderer, $rootScope) {
 
       function set(item) {
 
         var orientation = item.label.toUpperCase();
         stlCamera.orient(stlOrient.orient(stlOrient.orientations[orientation]));
         stlRenderer.render();
+
+        $rootScope.$apply();
       }
 
       return {
