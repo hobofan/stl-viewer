9913d02088eccdacc57b98581768c7291b24a4c9
Tathagata Chakraborty <tatx@me.com>
2015-10-02 11:45:37 +0530

    adding tests for the render styles

--- tests/services/rendering/model.js
+++ tests/services/rendering/model.js
@@ -6,4 +6,13 @@ describe('StlModel', function () {
   it('should have a open function', inject(function (stlModel) {
     expect(angular.isFunction(stlModel.open)).toBe(true);
   }));
+
+  it('should have a render styles enum and a function to set the render style',
+    inject(function (stlModel) {
+      expect(stlModel.renderStyles).toBeDefined();
+      expect(stlModel.renderStyles.SHADED).toBeDefined();
+      expect(stlModel.renderStyles.WIREFRAME).toBeDefined();
+      expect(stlModel.renderStyles.HARD_EDGES).toBeDefined();
+      expect(angular.isFunction(stlModel.setRenderStyle)).toBe(true);
+  }));
 });
